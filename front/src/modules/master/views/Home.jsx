63c6cac443a4243fe53fe33ebae0579638601ff9
JaelSantos01 <20223TN110@utez.edu.mx>
2025-04-15 18:06:54 -0600

    se agrego la carpeta para master

---
+++ front/src/modules/master/views/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return(
+        <>
+            Hola
+        </>
+    )
+}
+
+export default Home;
--- front/src/router/router.jsx
+++ front/src/router/router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Login from "../modules/auth/views/Login";
+import Home from "../modules/master/views/Home";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
