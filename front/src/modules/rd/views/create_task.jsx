5de1cc7a75cca736be052bee9ef04c11bb612394
Isaac22mo <20223tn135@utez.edu.mx>
2025-04-16 12:26:38 -0600

    uninstall react-router-dom

--- front/src/modules/rd/views/create_task.jsx
+++ front/src/modules/rd/views/create_task.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Sidebar from "../../../components/Sidebar.jsx";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 const TaskForm = () => {
     const navigate = useNavigate();
