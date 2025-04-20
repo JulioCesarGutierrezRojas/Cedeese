cf016f18fdfe9988ef9d129e89013b5282042771
Julio Cesar Gutierrez Rojas <20203tn160@utez.edu.mx>
2025-04-20 01:26:12 -0600

    UPDATE: move to next phase

--- back/src/main/java/utez/edu/mx/back/modules/phases/controller/PhaseController.java
+++ back/src/main/java/utez/edu/mx/back/modules/phases/controller/PhaseController.java
@@ -1,8 +1,11 @@
 package utez.edu.mx.back.modules.phases.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import utez.edu.mx.back.kernel.ApiResponse;
+import utez.edu.mx.back.modules.phases.controller.dto.MoveToNextPhaseDto;
 import utez.edu.mx.back.modules.phases.model.Phase;
 import utez.edu.mx.back.modules.phases.service.PhaseService;
 
@@ -11,37 +14,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/phases")
 @CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class PhaseController {
-    @Autowired
-    private PhaseService phaseService;
+    private final PhaseService phaseService;
 
-
-    @GetMapping
-    public ApiResponse<List<Phase>> getAllPhases() {
+    @GetMapping("/")
+    public ResponseEntity<Object> getAllPhases() {
         return phaseService.getAllPhases();
     }
 
-
-    @GetMapping("/{id}")
-    public ApiResponse<Phase> getPhaseById(@PathVariable Long id) {
-        return phaseService.getPhaseById(id);
-    }
-
-
-    @PostMapping
-    public ApiResponse<Phase> createPhase(@RequestBody Phase phase) {
-        return phaseService.savePhase(phase);
-    }
-
-
-    @PutMapping("/{id}")
-    public ApiResponse<Phase> updatePhase(@PathVariable Long id, @RequestBody Phase phase) {
-        return phaseService.updatePhase(id, phase);
-    }
-
-
-    @DeleteMapping("/{id}")
-    public ApiResponse<Void> deletePhase(@PathVariable Long id) {
-        return phaseService.deletePhase(id);
+    /**
+     * Avanza a la siguiente fase del proyecto si todas las tareas de la fase actual están completadas
+     * @param dto Objeto con el ID del proyecto y el ID de la fase actual
+     * @return ResponseEntity con el resultado de la operación
+     */
+    @PostMapping("/next-phase")
+    public ResponseEntity<Object> moveToNextPhase(@RequestBody MoveToNextPhaseDto dto) {
+        return phaseService.moveToNextPhase(dto.getProjectId(), dto.getCurrentPhaseId());
     }
 }
